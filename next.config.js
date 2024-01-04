13c15a09854ed71f2fcd20553839c310cd56c6a8
Eric Zhao <ericzh04@gmail.com>
2024-01-04 17:08:03 -0500

    Try again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
 
+module.exports = {
+    // Your other configurations...
+  
+    // Add this to enable static HTML export
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' },
+        // Add other paths as needed
+      };
+    },
+  };
+  
   
