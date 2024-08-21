883bb49aed0c2ad20d9a2af03d9001bb8eea4f08
Eric Zhao <ericzh04@gmail.com>
2024-08-20 21:38:09 -0400

    Update Snap date

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -14,7 +14,7 @@ const Experience = () => {
             <p className='text-xl tracking-widest uppercase text-[#89CFF0]'>Experience</p>
             <h2 className='py-4'>Where I&apos;ve worked</h2>
             <div className='grid md:grid-cols-2 gap-8'>
-                <ExperienceItem companyTitle={snapImg} jobTitle='Software Engineer Intern' dates='May 2024 - Present' description='Working on post-capture search in Lens Explorer so you can now search for and add filters, even after you have taken a snap.'/>
+                <ExperienceItem companyTitle={snapImg} jobTitle='Software Engineer Intern' dates='May 2024 - Aug 2024' description='Working on post-capture search in Lens Explorer so you can now search for and add filters, even after you have taken a snap.'/>
                 <ExperienceItem companyTitle={umichImg} jobTitle='Instructional Aide (EECS 203: Discrete Math)' dates='Aug 2023 - Present' description='I prepare and teach a weekly discussion of 25 students. I also hold weekly office hours, write homeworks and exam problems, and provide the professors with feedback on curriculum changes.'/>
                 <ExperienceItem companyTitle={verizonImg} jobTitle='Software Engineer Intern' dates='Jun 2023 - Aug 2023' description='I fined tuned a Large Language Model to build an internal querying tool trained on network data. Primarily worked in Python, HuggingFace, and Milvus Vector Database.'/>
                 <ExperienceItem companyTitle={westOrangeTennisClubImg} jobTitle='Tennis Coach' dates='Jun 2021 - Aug 2022' description='I coached tennis here for two summers, and met some incredible coworkers, athletes, and friends.'/>
